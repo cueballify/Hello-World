0c55ec759ab8fa8fc0ecf68a587bce48bf2d5303
cueball22 <cueball22@192.168.11.118>
2017-09-26 23:39:14 -0600

    Hello World

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+import javax.swing.*;
+/**
+ *
+ * @author cueball22
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+        JFrame myWindow;
+        myWindow=new JFrame();
+        
+        myWindow.setSize(300,200);
+        myWindow.setTitle("Beans");
+        myWindow.setVisible(true);
+        
+    }
+    
+}
